ed9d025f141f03d4b9146bc1b0f71ad4967f031c
Christopher Hewett <chewett@hotmail.co.uk>
2015-01-18 05:00:01 +0000

    Fixed the time

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import subprocess
 from datetime import datetime
 from datetime import timedelta
 
-tomorrow = "Jan 18 2015 02:36AM"
-today = "Jan 17 2015 02:35AM"
+tomorrow = "Jan 19 2015 02:37AM"
+today = "Jan 18 2015 02:36AM"
 
 #STOP DO NOT USE ABOVE VARIABLES IN YOUR LOOP
 #YOU WILL MAKE KITTENS SAD
