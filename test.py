d545fbd5a8086c5298202304496320b9d7903043
Christopher Hewett <chewett@hotmail.co.uk>
2017-10-20 05:00:01 +0100

    Fixed the time

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 
 #debug=False
 
-tomorrow = "Oct 20 2017 07:25PM"
-today = "Oct 19 2017 07:24PM"
+tomorrow = "Oct 21 2017 07:26PM"
+today = "Oct 20 2017 07:25PM"
 
 #STOP DO NOT USE ABOVE VARIABLES IN YOUR LOOP
 #YOU WILL MAKE KITTENS SAD
